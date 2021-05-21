5e461f2c03b25402432f09abadbca197e1a2b295
Jagadeesha B Y <jagadeey@amazon.com>
2021-05-21 16:26:54 -0700

    First commit

---
+++ client/src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Welcome Jagadeey
+        </p>
+       <h1>Cool</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
