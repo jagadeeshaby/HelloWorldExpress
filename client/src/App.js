639f349721e83ea20bd707cdb4f5cb6ca400ff1c
Jagadeesha B Y <jagadeey@amazon.com>
2021-05-21 18:48:58 -0700

    First code update

--- client/src/App.js
+++ client/src/App.js
@@ -6,10 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Welcome Jagadeey
-        </p>
-       <h1>Cool</h1>
+       <h1>Welcome to my app</h1>
       </header>
     </div>
   );
